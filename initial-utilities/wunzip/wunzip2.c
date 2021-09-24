901b35cafe4d970405bf4739c93d2d0673e4ca65
tr3p03lm <matiamukasa@outlook.com>
2021-09-24 08:12:33 +0000

    latest changes

--- initial-utilities/wunzip/wunzip2.c
+++ initial-utilities/wunzip/wunzip2.c
@@ -6,8 +6,6 @@
 #define floor
 #define log10
 
-
-
 int main(int agrc, char *argv[])
 {
     FILE *fileopen = fopen(argv[1], "r");
@@ -29,48 +27,75 @@ int main(int agrc, char *argv[])
     while (line_size > 0)
     {
         //printf("%d\n", atoi(line));
-        int j = 0;
-        char buffer[20];
-        int binary = atoi(line);
-        if (line[j] == '0'){
-            line[j] = '1';
-        }
-        int binaryfake = atoi(line);
-        sprintf(buffer, "%d", binaryfake);
-        printf("%s\n",buffer);
-
-        int decimal = 0,rem,base = 1;
-        int temp = binary;
-
-        while (temp > 0)
+        int found = 1;
+        for (int x = 0; x < found; x++)
         {
             /* code */
-            rem = temp % 10;;
-            decimal = decimal + rem * base;
-            temp = temp / 10;
-            base = base * 2;
+
+            int j = 0;
+            char buffer[200];
+            int binary = atoi(line);
+            if (line[j] == '0')
+            {
+                line[j] = '1';
+            }
+            int binaryfake = atoi(line);
+            sprintf(buffer, "%d", binaryfake);
+            printf("%s\n", buffer);
+
+            int decimal = 0, rem, base = 1;
+            int temp = binary;
+
+            while (temp > 0)
+            {
+                /* code */
+                rem = temp % 10;
+                ;
+                decimal = decimal + rem * base;
+                temp = temp / 10;
+                base = base * 2;
+            }
+
+            int nDigits = strlen(buffer);
+            while (nDigits < 1)
+            {
+                /* code */
+                nDigits++;
+            }
+
+            printf("The decimal number is: %d\n", decimal);
+            printf("The print stream is: ");
+            for (int i = 0; i < decimal; i++)
+            {
+                printf("%c", line[nDigits]);
+            }
+            printf("\n");
+            printf("%d is the length of the binary\n", nDigits);
+            printf("%c is the next character of the line\n", line[nDigits]);
+
+            char buf[strlen(line) + 1];
+            strncpy(buf, line + nDigits + 1, strlen(line) - nDigits);
+            printf("%s\n", buf);
+
+            if (line[nDigits] != '\0')
+            {
+                /* code */
+                //char buf[strlen(line) + 1];
+
+                // strncpy(buf, line + nDigits + 1, strlen(line) - nDigits);
+                // printf("%s\n", buf);
+                line = buf;
+                found++;
+            }
+            else
+            {
+                found--;
+            }
         }
 
-        int nDigits = strlen(buffer);
-        while (nDigits < 1)
-        {
-            /* code */
-            nDigits ++;
-        }
-        
-        
+        //line = buf;
 
-        printf("The decimal number is: %d\n", decimal);
-        printf("The print stream is: ");
-        for(int i = 0; i < decimal; i++){
-            printf("%c",line[nDigits]);
-        }
-        printf("\n");
-        printf("%d is the length of the binary\n", nDigits);
-        printf("%c is the next character of the line\n", line[nDigits]);
-        line_size = getline(&line, &line_buf_size, fileopen);
-
-        ///return line_size;
+        return 0;
     }
 
     return 0;
